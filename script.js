221a138c39de0df86f58c12520447b5069e5fb72
aliefezaa <89144143+aliefezaa@users.noreply.github.com>
2024-02-10 21:07:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+  document.getElementById('bukaKado').addEventListener('click', function() {
+    // Ganti URL gambar atau pesan kado sesuai keinginan Anda
+    var kadoURL = 'https://heyzine.com/flip-book/1333607348.html';
+
+    // Buka tautan dengan membuka kado
+    window.open(kadoURL);
+  });
